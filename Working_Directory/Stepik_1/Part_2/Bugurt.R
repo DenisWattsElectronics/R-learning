b96df3f9e158bc10efaa8f1f00232374daa85eea
dwe <ddomozhakov@mail.ru>
2022-01-26 17:41:17 +0300

    Stepik_R

--- Working_Directory/Stepik_1/Part_2/Bugurt.R
+++ Working_Directory/Stepik_1/Part_2/Bugurt.R
@@ -664,6 +664,151 @@ str(attitude)
 summary(lm(rating ~ complaints * critical, attitude))
 confint(lm(rating ~ complaints * critical, attitude))
 
+###
+# categorical predictors
+str(swiss)
+swiss
+hist(swiss$Catholic, col = 'red')
+
+swiss$religious <- ifelse(swiss$Catholic > 60, 'Lots', 'Few')
+swiss$religious <- as.factor(swiss$religious)
+
+fit3 <- lm(Fertility ~ Examination + religious, data = swiss)
+summary(fit3)
+
+fit4 <- lm(Fertility ~ Examination*religious, data = swiss)
+fit4 <- lm(Fertility ~ religious*Examination, data = swiss)
+summary(fit4)
+
+# plots
+
+ggplot(swiss, aes(x = Examination, y = Fertility)) + 
+  geom_point() 
+
+ggplot(swiss, aes(x = Examination, y = Fertility)) + 
+  geom_point() + 
+  geom_smooth()
+
+ggplot(swiss, aes(x = Examination, y = Fertility)) + 
+  geom_point() + 
+  geom_smooth(method = 'lm')
+
+ggplot(swiss, aes(x = Examination, y = Fertility, col = religious)) + 
+  geom_point() 
+
+ggplot(swiss, aes(x = Examination, y = Fertility, col = religious)) + 
+  geom_point()  + 
+  geom_smooth()
+
+ggplot(swiss, aes(x = Examination, y = Fertility, col = religious)) + 
+  geom_point()  + 
+  geom_smooth(method = 'lm')
+
+#####
+dfmtc <- mtcars
+dfmtc$am <- factor(mtcars$am, labels = c('Automatic', 'Manual'))
+dfmtc$wt_centered <- mtcars$wt - mean(mtcars$wt)
+
+fit <- lm(mpg ~ wt_centered*am, dfmtc)
+summary(fit)
+
+ggplot(dfmtc, aes(x = wt, y = mpg, col = am)) + 
+  geom_point()  + 
+  geom_smooth(method = 'lm')
+
+ggplot(dfmtc, aes(x = wt, y = mpg)) + 
+  geom_point()  + 
+  geom_smooth(method = 'lm')
+
+###
+
+rm(swiss)
+str(swiss)
+
+fit5 <- lm(Fertility ~ religious*Infant.Mortality*Examination, data = swiss)
+summary(fit5)
+
+
+# model comparison
+
+rm(swiss)
+swiss <- data.frame(swiss)
+
+fit_full <- lm(Fertility ~ ., data = swiss)
+summary(fit_full)
+
+fit_reduced1 <- lm(Fertility ~ Infant.Mortality + Examination + Catholic + Education, data = swiss)
+summary(fit_reduced1)
+
+anova(fit_full, fit_reduced1)
+
+fit_reduced2 <- lm(Fertility ~ Infant.Mortality + Education + Catholic + Agriculture, data = swiss)
+summary(fit_reduced2)
+
+anova(fit_full, fit_reduced2)
+
+fit_reduced3 <-lm(Fertility ~ . -Agriculture -Education, swiss)
+
+# model selection
+
+optimal_fit <-  step(fit_full, direction = 'backward')
+summary(optimal_fit)
+
+#####
+model_full <- lm(rating ~ ., data = attitude) 
+model_null <- lm(rating ~ 1, data = attitude)
+scope = list(lower = model_null, upper = model_full)
+
+ideal_model <- step(model_full, direction = 'backward', scope = scope)
+summary(ideal_model)
+
+anova(model_full, ideal_model)
+
+###
+
+pairs(swiss)
+ggplot(swiss, aes(x = Examination, y = Education)) + 
+  geom_point()
+
+
+# Outliers
+
+ggplot(swiss, aes(x = Examination, y = Education)) + 
+  geom_point() + 
+  geom_smooth(method = 'lm')
+
+#####
+
+my_vector <- c(0.027, 0.079, 0.307, 0.098, 0.021, 0.091, 0.322, 0.211, + 0.069, 0.261, 0.241, 0.166, 0.283, 0.041, 0.369, 0.167, 0.001, 0.053, 0.262, 0.033, 0.457, 0.166, 0.344, 0.139, 0.162, 0.152, 0.107, 0.255, 0.037, 0.005, 0.042, 0.220, 0.283, 0.050, 0.194, 0.018, 0.291, 0.037, 0.085, 0.004, 0.265, 0.218, 0.071, 0.213, 0.232, 0.024, 0.049, 0.431, 0.061, 0.523)
+shapiro.test(my_vector)
+shapiro.test(1/my_vector)
+shapiro.test(sqrt(my_vector))
+shapiro.test(log(my_vector))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
