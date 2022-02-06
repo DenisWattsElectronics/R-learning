523bbd6d81f680d9ca2ba714d9384d0ca834187d
dwe <ddomozhakov@mail.ru>
2022-02-06 20:12:38 +0300

    R_Stepik_AA_Done

--- Working_Directory/R programming fundamentals/Bugurt.R
+++ Working_Directory/R programming fundamentals/Bugurt.R
@@ -75,9 +75,127 @@ height_variables
 sort(sapply(evian1[height_variables], max), decreasing = T)
 
 
+library(stringr)
+
+hamlet <- "To be, or not to be: that is the question:
+Whether 'tis nobler in the mind to suffer
+The slings and arrows of outrageous fortune,
+Or to take arms against a sea of troubles,
+And by opposing end them?"
+
+hamlet <- str_replace_all(hamlet, "[:punct:]", "")
+hamlet <- tolower(unlist(str_split(hamlet, "[:space:]")))
+hamlet
+
+sum(str_count(string = hamlet, pattern = )) 
+5 + 1 + 2
+sum(hamlet=="to")
+sum(grepl("[fqw]", hamlet))
+sum(grepl("b.", hamlet))
+sum(nchar(hamlet)==7)
+
+sum(str_detect(hamlet,"to"))
+sum(str_detect(hamlet,"[fqw]"))
+sum(str_detect(hamlet,"b."))
+sum(str_length(hamlet)==7)
+
+?quakes
+str(quakes)
+summary(quakes$mag)
+cut(quakes$mag, breaks = 10, right = T)
+table(cut(quakes$mag, breaks = c(4, 4.5, 5, 5.5, 6, 6.5), left = T))
+table(quakes$mag)
+
+sort(table(cut(quakes$mag, seq(4, 6.5, by = 0.5), r = F)), d = T)
+cat(names(sort(table(cut(quakes$mag, seq(4, 6.5, by = 0.5), right = F)), decreasing = T)))
+sort(table(cut(quakes$mag, seq(4, 6.5, 0.5), right = F)), decreasing = T)
+  
+
+
+#install.packages('stringr')
+library(stringr)
+df <- read.csv("https://raw.githubusercontent.com/tonytonov/Rcourse/master/R%20programming/avianHabitat.csv")
+#отбираем только те столбцы, у которых встречается P
+df2 <- df[names(df)[str_detect(names(df),"^P")]]
+#добавляем суммы по строчкам
+df2$total_coverage <- apply(df2,1,sum)
+#удаляем цифры из значений и даем новой переменной
+df2$site_name <- str_replace(df$Site, "[:digit:]+", "")
+#считаем среднее для видов
+df2$site_name <- factor(df2$site_name)
+sort(tapply(df2$total_coverage, df2$site_name, mean))
+
+x <- read.csv("https://raw.githubusercontent.com/tonytonov/Rcourse/master/R%20programming/avianHabitat.csv")
+x$total <- rowSums(x[grepl("^P.", names(x))])
+x$sname <- factor(gsub("[0-9]", "", as.character(x$Site)))
+which.min(tapply(x$total, x$sname, mean))
 
 
 
+sapply (names(df)[str_detect(names(df), "Ht")], function(x) tapply(x, df$Observer, max))
+tapply(Ht, Observer, max)
+
+
+sapply(avian[, grepl("Ht", names(avian))], function(name) {tapply(name, avian$Observer, max)})
+
+
+height_variables <- names(avian)[str_detect(names(avian), "Ht$")]
+sapply(height_variables, function(x) names(which.max(tapply(avian[[x]], avian$Observer, max))))
+
+"%+%" <- function(x, y) {
+  l <- c(length(x), length(y))
+  length(x) <- length(y) <- min(l)
+  s <- x + y
+  return(s[1:max(l)])
+  }
+1:5 %+% 1:2
+
+
+
+
+
+
+
+
+
+
+
+# Random walk with absorption
+simulate_walk <- function(maxr = 6, n_max = 100, p = 1e-2) {
+  current_position <- c(0, 0)
+  for (i in 1:n_max) {
+    is_absorbed <- rbinom(1, 1, p)
+    if (is_absorbed) return(list(status = "Absorbed"))
+    current_position <- c(current_position[1] + rnorm(1), current_position[2] + rnorm(1))
+    if (sqrt(current_position[1]^2 + current_position[2]^2) > maxr) return(list(status = "Breach"))
+  }
+  return(list(status = "Max steps reached", 
+              position = current_position,
+              steps = n_max))
+}
+
+# Simulate results
+simulate_walk()
+result <- replicate(1000000, simulate_walk(), simplify = FALSE)
+result <- data.frame(
+  status = sapply(result, function(x) x$status)
+)
+
+
+head(result)
+
+# Inspect results
+tapply(result$status)
+tapply(result$steps, result$status, mean)
+
+summary(result)
+
+
+funs <- c("print","summary","plot")
+meths <- lapply(funs, methods)
+grepl("matrix", meths)
+grepl("function", meths)
+grepl("default", meths)
 
 
 
