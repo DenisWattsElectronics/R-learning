feeb73b14632dbcc9cca70f6e9afed36103fa42b
dwe <ddomozhakov@mail.ru>
2022-02-20 18:57:23 +0300

    R_Stepik_2

--- Working_Directory/Stepik_2/Bugurt.R
+++ Working_Directory/Stepik_2/Bugurt.R
@@ -192,3 +192,75 @@ get_p_value <- function(test_list){
 }
 
 
+
+library(ggplot2)
+library(stats)
+library(dplyr)
+
+seq(0, 1, length.out = 11)
+seq(stats::rnorm(20)) # effectively 'along'
+seq(1, 9, by = 2)     # matches 'end'
+seq(1, 9, by = pi)    # stays below 'end'
+seq(1, 6, by = 3)
+seq(1.575, 5.125, by = 0.05)
+seq(17) # same as 1:17, or even better seq_len(17)
+
+seq(1,length(diamonds[[1]]),2)
+seq(2,nrow(diamonds),2)
+
+diamonds = tibble(diamonds)
+
+filter(diamonds, carat > 0.3 | color == "J")
+rownames(diamonds)
+
+filter(diamonds, as.integer(rownames(diamonds))%%2!=0)
+slice(diamonds, seq(1, nrow(diamonds), 2))
+
+select
+slice
+filter
+arrange
+mutate
+rename
+
+my_df <- mtcars %>% 
+  select(mpg, hp, am, vs) %>% 
+  filter(mpg > 14 & hp > 100) %>% 
+  arrange(-mpg) %>% 
+  slice(1:10) %>% 
+  rename("Miles per gallon" = mpg, "Gross horsepower" = hp)
+
+
+my_data <- data_frame(x = rnorm(10000), y = rnorm(10000), 
+                      f = factor(rep(1:2, 5000)))
+
+log_transform <- function(test_data){
+  mutate_each(test_data, funs((. - max(.))/(max(.) - min(.)) + 1))
+  return(mutate_each(test_data, funs(log(.))))
+}
+
+log_transform(my_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
