4fd049b1f87483d34872555d0c75ae93367bb81a
dwe <ddomozhakov@mail.ru>
2022-03-25 10:12:26 +0300

    R GUM

--- Working_Directory/Random Gym/iris_games.R
+++ Working_Directory/Random Gym/iris_games.R
@@ -63,58 +63,67 @@ shapiro.test(iris$Petal.Length)
 shapiro.test(iris$Petal.Width)
 
 # Histograms for all W/L ans species all
-par(
-  mfrow=c(4,3),
-  mar=c(4,4,1,0)
-)
-hist(df$Sepal.Length[df$Species == 'setosa'], breaks = 10, xlab = "PL", main ="Sepal.Length setosa", 
-     col = "white", cex.lab = 1.2, cex.axis = 1.2)
-hist(df$Sepal.Length[df$Species == 'versicolor'], breaks = 10, xlab = "PL", main ="Sepal.Length versicolor", 
-     col = "white", cex.lab = 1.2, cex.axis = 1.2)
-hist(df$Sepal.Length[df$Species == 'virginica'], breaks = 10, xlab = "PL", main ="Sepal.Length virginica", 
-     col = "white", cex.lab = 1.2, cex.axis = 1.2)
-hist(df$Petal.Length[df$Species == 'setosa'], breaks = 10, xlab = "PL", main ="Petal.Length setosa", 
-     col = "white", cex.lab = 1.2, cex.axis = 1.2)
-hist(df$Petal.Length[df$Species == 'versicolor'], breaks = 10, xlab = "PL", main ="Petal.Length versicolor", 
-     col = "white", cex.lab = 1.2, cex.axis = 1.2)
-hist(df$Petal.Length[df$Species == 'virginica'], breaks = 10, xlab = "PL", main ="Petal.Length virginica", 
-     col = "white", cex.lab = 1.2, cex.axis = 1.2)
-hist(df$Sepal.Width[df$Species == 'setosa'], breaks = 10, xlab = "PL", main ="Sepal.Width setosa", 
-     col = "white", cex.lab = 1.2, cex.axis = 1.2)
-hist(df$Sepal.Width[df$Species == 'versicolor'], breaks = 10, xlab = "PL", main ="Sepal.Width versicolor", 
-     col = "white", cex.lab = 1.2, cex.axis = 1.2)
-hist(df$Sepal.Width[df$Species == 'virginica'], breaks = 10, xlab = "PL", main ="Sepal.Width virginica", 
-     col = "white", cex.lab = 1.2, cex.axis = 1.2)
-hist(df$Petal.Width[df$Species == 'setosa'], breaks = 10, xlab = "PL", main ="Petal.Width setosa", 
-     col = "white", cex.lab = 1.2, cex.axis = 1.2)
-hist(df$Petal.Width[df$Species == 'versicolor'], breaks = 10, xlab = "PL", main ="Petal.Width versicolor", 
-     col = "white", cex.lab = 1.2, cex.axis = 1.2)
-hist(df$Petal.Width[df$Species == 'virginica'], breaks = 10, xlab = "PL", main ="Petal.Width virginica", 
-     col = "white", cex.lab = 1.2, cex.axis = 1.2)
-par(mfrow=c(1,1))
+
+iris_hist_all <- function() {
+  par(
+    mfrow=c(4,3),
+    mar=c(4,4,1,0)
+  )
+  hist(df$Sepal.Length[df$Species == 'setosa'], breaks = 10, xlab = "PL", main ="Sepal.Length setosa", 
+       col = "white", cex.lab = 1.2, cex.axis = 1.2)
+  hist(df$Sepal.Length[df$Species == 'versicolor'], breaks = 10, xlab = "PL", main ="Sepal.Length versicolor", 
+       col = "white", cex.lab = 1.2, cex.axis = 1.2)
+  hist(df$Sepal.Length[df$Species == 'virginica'], breaks = 10, xlab = "PL", main ="Sepal.Length virginica", 
+       col = "white", cex.lab = 1.2, cex.axis = 1.2)
+  hist(df$Petal.Length[df$Species == 'setosa'], breaks = 10, xlab = "PL", main ="Petal.Length setosa", 
+       col = "white", cex.lab = 1.2, cex.axis = 1.2)
+  hist(df$Petal.Length[df$Species == 'versicolor'], breaks = 10, xlab = "PL", main ="Petal.Length versicolor", 
+       col = "white", cex.lab = 1.2, cex.axis = 1.2)
+  hist(df$Petal.Length[df$Species == 'virginica'], breaks = 10, xlab = "PL", main ="Petal.Length virginica", 
+       col = "white", cex.lab = 1.2, cex.axis = 1.2)
+  hist(df$Sepal.Width[df$Species == 'setosa'], breaks = 10, xlab = "PL", main ="Sepal.Width setosa", 
+       col = "white", cex.lab = 1.2, cex.axis = 1.2)
+  hist(df$Sepal.Width[df$Species == 'versicolor'], breaks = 10, xlab = "PL", main ="Sepal.Width versicolor", 
+       col = "white", cex.lab = 1.2, cex.axis = 1.2)
+  hist(df$Sepal.Width[df$Species == 'virginica'], breaks = 10, xlab = "PL", main ="Sepal.Width virginica", 
+       col = "white", cex.lab = 1.2, cex.axis = 1.2)
+  hist(df$Petal.Width[df$Species == 'setosa'], breaks = 10, xlab = "PL", main ="Petal.Width setosa", 
+       col = "white", cex.lab = 1.2, cex.axis = 1.2)
+  hist(df$Petal.Width[df$Species == 'versicolor'], breaks = 10, xlab = "PL", main ="Petal.Width versicolor", 
+       col = "white", cex.lab = 1.2, cex.axis = 1.2)
+  hist(df$Petal.Width[df$Species == 'virginica'], breaks = 10, xlab = "PL", main ="Petal.Width virginica", 
+       col = "white", cex.lab = 1.2, cex.axis = 1.2)
+  par(mfrow=c(1,1))
+}
+
+iris_hist_all()
 
 # Normality check all
 
-rbind(
-c("SL, setosa", shapiro.test(iris$Sepal.Length[iris$Species == 'setosa'])$p.value),
-c("SL, versicolor", shapiro.test(iris$Sepal.Length[iris$Species == 'versicolor'])$p.value),
-c("SL, virginica", shapiro.test(iris$Sepal.Length[iris$Species == 'virginica'])$p.value),
+iris_norma_check_all <- function() {
+  rbind(
+    c("SL, setosa", shapiro.test(iris$Sepal.Length[iris$Species == 'setosa'])$p.value),
+    c("SL, versicolor", shapiro.test(iris$Sepal.Length[iris$Species == 'versicolor'])$p.value),
+    c("SL, virginica", shapiro.test(iris$Sepal.Length[iris$Species == 'virginica'])$p.value),
+    
+    c("PL, setosa", shapiro.test(iris$Petal.Length[iris$Species == 'setosa'])$p.value),
+    c("PL, versicolor", shapiro.test(iris$Petal.Length[iris$Species == 'versicolor'])$p.value),
+    c("PL, virginica", shapiro.test(iris$Petal.Length[iris$Species == 'virginica'])$p.value),
+    
+    c("SW, setosa", shapiro.test(iris$Sepal.Width[iris$Species == 'setosa'])$p.value),
+    c("SW, versicolor", shapiro.test(iris$Sepal.Width[iris$Species == 'versicolor'])$p.value),
+    c("SW, virginica", shapiro.test(iris$Sepal.Width[iris$Species == 'virginica'])$p.value),
+    
+    c("PW, setosa", shapiro.test(iris$Petal.Width[iris$Species == 'setosa'])$p.value),
+    c("PW, versicolor", shapiro.test(iris$Petal.Width[iris$Species == 'versicolor'])$p.value),
+    c("PW, virginica", shapiro.test(iris$Petal.Width[iris$Species == 'virginica'])$p.value) )
+}
 
-c("PL, setosa", shapiro.test(iris$Petal.Length[iris$Species == 'setosa'])$p.value),
-c("PL, versicolor", shapiro.test(iris$Petal.Length[iris$Species == 'versicolor'])$p.value),
-c("PL, virginica", shapiro.test(iris$Petal.Length[iris$Species == 'virginica'])$p.value),
+iris_norma_check_all()
 
-c("SW, setosa", shapiro.test(iris$Sepal.Width[iris$Species == 'setosa'])$p.value),
-c("SW, versicolor", shapiro.test(iris$Sepal.Width[iris$Species == 'versicolor'])$p.value),
-c("SW, virginica", shapiro.test(iris$Sepal.Width[iris$Species == 'virginica'])$p.value),
-
-c("PW, setosa", shapiro.test(iris$Petal.Width[iris$Species == 'setosa'])$p.value),
-c("PW, versicolor", shapiro.test(iris$Petal.Width[iris$Species == 'versicolor'])$p.value),
-c("PW, virginica", shapiro.test(iris$Petal.Width[iris$Species == 'virginica'])$p.value) )
-
-
-
-
+# t-test, but in this case it isn't useful due to three levels in Species 
+df1 <- subset(df, Species != "setosa")
+t.test(Sepal.Length ~ Species, df1)
 
 
 
